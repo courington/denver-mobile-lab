a72f29abfa3d32a3a0f5bed5af8dced87b72c269
Chase <chase.courington@gmail.com>
2012-10-12 22:52:11 -0600

    setting up the rails project

---
+++ den_molab/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DenMolab::Application.config.secret_token = '724ad075303d624ea4e001b3506dd4e158597a2e4016d82eb92a48ef436006e9761181837cabf315270d3212759ff03f84168fddf8eaee9052a639b916222a61'
